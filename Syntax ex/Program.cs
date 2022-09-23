3c554d1b4d6c6bde25324d5d028039df49284c25
Amir Gugssa <amirgugssa@gmail.com>
2022-09-23 01:26:12 -0400

    finished syntax

---
+++ Syntax ex/Program.cs
@@ -0,0 +1,19 @@
+﻿var answer = 4;
+
+var answer1 = (answer < 9) ? $"{answer} is less than 9" : $"{answer} is greater than 9";
+
+Console.WriteLine(answer1);
+
+
+
+
+
+
+//if (answer < 9)
+//{
+//    response = answer + " is less than nine";
+//}
+//else
+//{
+//    response = answer + " is greater than or equal to nine";
+//}
